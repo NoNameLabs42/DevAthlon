ba3109a182ce19e9f1aa6d5027a1d293173514ff
Lau <NoNameLabs42@googlemail.com>
2015-07-11 01:34:10 +0200

    Scoreboard etc

--- DevAthlon/src/de/nonamelabs/devathlon/paccraft/Game.java
+++ DevAthlon/src/de/nonamelabs/devathlon/paccraft/Game.java
@@ -1,7 +1,9 @@
 package de.nonamelabs.devathlon.paccraft;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
@@ -19,7 +21,9 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -32,7 +36,11 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
+import de.nonamelabs.devathlon.Plugin_DevAthlon;
 import de.nonamelabs.devathlon.paccraft.items.Armor;
 import de.nonamelabs.devathlon.paccraft.items.Items;
 import de.nonamelabs.devathlon.paccraft.items.LootBoxen;
@@ -45,10 +53,14 @@ public class Game implements Listener{
 	public static final Material COIN_MATERIAL = Material.GOLD_NUGGET;
 	public static final Material COIN_BOTTOM_BLOCK = Material.BARRIER;	
 	public static final Material COIN_BOTTOM_BLOCK2 = Material.WOOD;	
+	public static final int TOTAL_TIME = 300;
 	
 	public List<Player> player_list = new ArrayList<Player>();
 	public List<Player> ghost_list = new ArrayList<Player>();
 	public List<Player> spectator_list = new ArrayList<Player>();
+	public Map<Player, Scoreboard> scoreboards = new HashMap<Player, Scoreboard>();
+	public Map<Player, Integer> scores = new HashMap<Player, Integer>();
+	public List<Location> playerspawns;
 	
 	public int players;
 	public int ghosts;
@@ -56,9 +68,19 @@ public class Game implements Listener{
 	public Random r = new Random();
 	public Location mapcorner1;
 	public Location mapcorner2;	
+	public String remainingTime;
+	public boolean task_running = true;
+	
 	
 	public Game(int players, int ghosts, List<Location> playerspawns, Location ghostspawn, Location mapcorner1, Location mapcorner2) {
+		for (Player p: Bukkit.getOnlinePlayers()) {
+			Scoreboard sc = Bukkit.getScoreboardManager().getNewScoreboard();
+			scoreboards.put(p, sc);
+			p.setScoreboard(sc);
+		}
+		
 		for (Player p:Bukkit.getOnlinePlayers()) {
+					
 			if (r.nextBoolean()) {
 				if (player_list.size() < players) {
 					player_list.add(p);
@@ -78,7 +100,7 @@ public class Game implements Listener{
 					player_list.add(p);
 					initPlayer(p, playerspawns.get(player_list.size()-1));
 				} else {
-				spectator_list.add(p);
+					spectator_list.add(p);
 					initSpectator(p, ghostspawn);
 				}
 			}
@@ -89,8 +111,38 @@ public class Game implements Listener{
 		this.ghostspawn = ghostspawn;
 		this.mapcorner1 = mapcorner1;
 		this.mapcorner2 = mapcorner2;
+		this.playerspawns = playerspawns;
 		
 		initMap();
+		
+		Bukkit.getScheduler().runTaskAsynchronously(Plugin_DevAthlon.Instance, new Runnable() {
+			
+			@Override
+			public void run() {
+				for (int t = 1; t <= TOTAL_TIME && task_running; t++) {
+					int time = TOTAL_TIME - t;
+					String seconds = String.valueOf(time % 60);
+					for (int i = 0; i < 2 - seconds.length(); i++) {
+						seconds = "0" + seconds;
+					}
+					remainingTime = (time / 60) + ":" + seconds;
+					
+					if ((t % 30) == 0) {
+						sendGameMessage(remainingTime + " verbleibend!");
+					}
+					
+					updateScoreboards();
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				//TODO: finish Game!
+			}
+		});
 	}
 	
 	public void initMap() {
@@ -125,6 +177,7 @@ public class Game implements Listener{
 		p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999, 4));
 		p.setDisplayName(ChatColor.YELLOW + p.getName());
 		p.setPlayerListName(ChatColor.YELLOW + p.getName());
+		scores.put(p, 0);
 		
 		PlayerInventory inv = p.getInventory();	
 		inv.clear();	
@@ -132,6 +185,7 @@ public class Game implements Listener{
 		inv.setItem(8, Items.Shop());
 		
 		sendGameMessage("Du bist ein Spieler!", p);
+		updateScoreboards();
 	}
 	
 	public void initGhost(Player p, Location l) {
@@ -145,14 +199,15 @@ public class Game implements Listener{
 		p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999, 2));
 		p.setDisplayName(ChatColor.WHITE + p.getName());
 		p.setPlayerListName(ChatColor.WHITE + p.getName());
-
+		scores.put(p, 0);
+		
 		PlayerInventory inv = p.getInventory();	
 		inv.clear();	
 		inv.setArmorContents(new ItemStack[] {Armor.GhostBoots(), Armor.GhostLegs(), Armor.GhostChest(), Armor.GhostHelm()});
 		inv.setItem(8, Items.Shop());
 		
-		
 		sendGameMessage("Du bist ein Geist!", p);
+		updateScoreboards();
 	}
 	
 	public void initSpectator(Player p, Location l) {
@@ -164,7 +219,80 @@ public class Game implements Listener{
 		p.setFoodLevel(20);
 		p.setExp(0);
 		p.setLevel(0);
+		
 		sendGameMessage("Das Spiel ist bereits voll! Du bist nun ein Spectator!", p);
+		updateScoreboard(p);
+	}
+	
+	public void updateScoreboards() {
+		for (Player p: Bukkit.getOnlinePlayers()) {
+			updateScoreboard(p);
+		}
+	}
+	
+	public void updateScoreboard(Player p) {
+		Scoreboard sc = scoreboards.get(p);
+		Objective obj;
+		
+		if (sc.getObjective("PacCraft") == null) {
+			obj = sc.registerNewObjective("PacCraft", "dummy");
+		} else {
+			obj = sc.getObjective("PacCraft");
+			obj.unregister();
+			obj = sc.registerNewObjective("PacCraft", "dummy");
+		}
+		
+		obj.setDisplayName(PLUGIN_NAME_COLOR + "PacCraft");
+		obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+		
+		int curr = 0;
+		if (player_list.contains(p)) {
+			obj.getScore(PLUGIN_COLOR + "Score: " + scores.get(p)).setScore(curr++);
+			obj.getScore(PLUGIN_NAME_COLOR + "Spieler").setScore(curr++);
+		} else if (ghost_list.contains(p)) {
+			obj.getScore(PLUGIN_COLOR + "Score: " + scores.get(p)).setScore(curr++);
+			obj.getScore(PLUGIN_NAME_COLOR + "Geist").setScore(curr++);
+		} else if (spectator_list.contains(p)) {
+			obj.getScore(PLUGIN_NAME_COLOR + "Zuschauer").setScore(curr++);
+		}
+		obj.getScore(PLUGIN_COLOR + "---------------").setScore(curr++);
+		obj.getScore(PLUGIN_COLOR + "Geister: " + ghost_list.size()).setScore(curr++);
+		obj.getScore(PLUGIN_COLOR + "Spieler: " + player_list.size()).setScore(curr++);
+		obj.getScore(ChatColor.RESET + "" + PLUGIN_COLOR + "---------------").setScore(curr++);
+		
+		Player p1 = null, p2 = null, p3 = null;
+		
+		for (Player player: player_list) {
+			if (p1 == null || scores.get(p1) < scores.get(player)) {
+				p3 = p2;
+				p2 = p1;
+				p1 = player;
+			} else if (p2 == null || scores.get(p2) < scores.get(player)) {
+				p3 = p2;
+				p2 = player;
+			} else if (p3 == null || scores.get(p3) < scores.get(player)) {
+				p3 = player;
+			}
+		}
+		
+		for (Player player: ghost_list) {
+			if (p1 == null || scores.get(p1) < scores.get(player)) {
+				p3 = p2;
+				p2 = p1;
+				p1 = player;
+			} else if (p2 == null || scores.get(p2) < scores.get(player)) {
+				p3 = p2;
+				p2 = player;
+			} else if (p3 == null || scores.get(p3) < scores.get(player)) {
+				p3 = player;
+			}
+		}
+		
+		if (p3 != null)	obj.getScore(ChatColor.DARK_GRAY + "3. " + p3.getName() + " - " + scores.get(p3)).setScore(curr++);
+		if (p2 != null) obj.getScore(ChatColor.GRAY + "2. " + p2.getName() + " - " + scores.get(p2)).setScore(curr++);		
+		if (p1 != null) obj.getScore(ChatColor.GOLD + "1. " + p1.getName() + " - " + scores.get(p1)).setScore(curr++);
+		obj.getScore(ChatColor.RESET + "" + ChatColor.RESET + PLUGIN_COLOR + "---------------").setScore(curr++);
+		obj.getScore(PLUGIN_COLOR + "Zeit: " + remainingTime).setScore(curr++);
 	}
 	
 	public void sendGameMessage(String message) {
@@ -190,11 +318,17 @@ public class Game implements Listener{
 			p.setPlayerWeather(WeatherType.CLEAR);
 			p.removePotionEffect(PotionEffectType.BLINDNESS);
 			p.getInventory().clear();
+			p.getInventory().setArmorContents(new ItemStack[4]);
+			
+			p.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
 		}
+		
+		task_running = false;
 	}
 	//Events-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
+		event.setJoinMessage("");
 		spectator_list.add(event.getPlayer());
 		initSpectator(event.getPlayer(), ghostspawn);
 	}
@@ -205,20 +339,22 @@ public class Game implements Listener{
 		if (ghost_list.contains(event.getPlayer())) {
 			ghost_list.remove(event.getPlayer());
 			sendGameMessage(event.getPlayer().getName() + " hat das Spiel verlassen! Es gibt noch " + ghost_list.size() + " Geister!");
+			updateScoreboards();
 		} else if (player_list.contains(event.getPlayer())) {
 			player_list.remove(event.getPlayer());
 			sendGameMessage(event.getPlayer().getName() + " hat das Spiel verlassen! Es gibt noch " + player_list.size() + " Spieler!");
+			updateScoreboards();
 		} else {
 			spectator_list.remove(event.getPlayer());
 		}
+		scoreboards.remove(event.getPlayer());
 	}
 	
 	
 	@EventHandler
 	public void onPlayerClick(PlayerInteractEvent event){
-		event.setCancelled(true);
-		
 		if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getItem() != null) {
+			event.setCancelled(true);
 			if (event.getItem().equals((Items.Shop()))) {
 				event.setCancelled(true);
 				event.getPlayer().openInventory(LootBoxen.getShopInventory());
@@ -254,12 +390,15 @@ public class Game implements Listener{
 	
 	@EventHandler
 	public void onPlayerDamage(EntityDamageEvent event) {
+		
 		event.setCancelled(true);
 	}
 	
 	@EventHandler
 	public void onPlayerLooseHunger(FoodLevelChangeEvent event) {
-		event.setCancelled(true);
+		if (!player_list.contains((Player) event.getEntity())) {
+			event.setCancelled(true);
+		}
 	}
 	
 	@EventHandler
@@ -289,9 +428,47 @@ public class Game implements Listener{
 	public void onPlayerPickup(PlayerPickupItemEvent event) {
 		Player p = event.getPlayer();
 		if (player_list.contains(p) && event.getItem() != null && event.getItem().getItemStack().getType() == COIN_MATERIAL) {
-			p.playSound(p.getLocation(), Sound.LEVEL_UP, 1, 1);
+			p.playSound(p.getLocation(), Sound.ORB_PICKUP, 1, 1);
 			event.getItem().remove();
+			
+			int coins = 0;
+			int random = r.nextInt(10);
+			if (random < 6) {
+				coins = 1;
+			} else if (random < 9){
+				coins = 2;
+			} else {
+				coins = 3;
+			}
+			scores.put(p, scores.get(p) + coins);
+			updateScoreboard(p);
 		}
 		event.setCancelled(true);
 	}	
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		final Player p = event.getEntity();
+		event.setDeathMessage("");
+		event.setKeepInventory(true);
+		event.setDroppedExp(0);
+		
+		Location l = null;
+		if (ghost_list.contains(p)) {
+			l = ghostspawn;
+		} else if (player_list.contains(p)) {
+			l = playerspawns.get(r.nextInt(playerspawns.size()));
+		}
+		final Location spawnpoint = l;
+		
+		
+		Bukkit.getScheduler().runTaskLater(Plugin_DevAthlon.Instance, new Runnable() {
+			
+			@Override
+			public void run() {
+				p.spigot().respawn();
+				p.teleport(spawnpoint);
+			}
+		}, 60L);
+	}
 }
