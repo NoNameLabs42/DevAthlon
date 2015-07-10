a09fc2845774a40ab81fad352f47b4ce789f26ed
Lau <NoNameLabs42@googlemail.com>
2015-07-10 23:11:39 +0200

    Push

--- DevAthlon/src/de/nonamelabs/devathlon/Plugin_DevAthlon.java
+++ DevAthlon/src/de/nonamelabs/devathlon/Plugin_DevAthlon.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -11,6 +12,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 
 import de.nonamelabs.devathlon.paccraft.Game;
@@ -24,6 +26,7 @@ public class Plugin_DevAthlon extends JavaPlugin{
 	public List<Location> playerspawns = new ArrayList<Location>();  
 	public Location ghostspawn;
 	public int registered_playerspawns;
+	public Location mapcorner1, mapcorner2;
 	
 	//Spiel
 	public Game game;
@@ -37,6 +40,11 @@ public class Plugin_DevAthlon extends JavaPlugin{
 	}
 	
 	public void onDisable() {
+		if (game != null) {
+			game.stopGame();
+			game = null;
+		}
+		
 		save_Config();
 		
 		logger.info("Plugin wurde gestoppt!");
@@ -47,7 +55,8 @@ public class Plugin_DevAthlon extends JavaPlugin{
     	
     	ghostspawn = (Location) config.get("Geisterspawn");
     	registered_playerspawns = config.getInt("Playerspawnanzahl");
-    	
+    	mapcorner1 = (Location) config.get("MapEcke1");
+    	mapcorner2 = (Location) config.get("MapEcke2");
     	for (int i = 0; i < registered_playerspawns; i++) {
     		playerspawns.add((Location) config.get("Playerspawn" + i));
     	}
@@ -59,6 +68,9 @@ public class Plugin_DevAthlon extends JavaPlugin{
     	FileConfiguration config = getConfig();
     	
     	config.set("Geisterspawn", ghostspawn);
+    	config.set("MapEcke1", mapcorner1);
+    	config.set("MapEcke2", mapcorner2);
+    	
     	registered_playerspawns = playerspawns.size();
     	config.set("Playerspawnanzahl", registered_playerspawns);
     	for (int i = 0; i < registered_playerspawns; i++) {
@@ -106,6 +118,36 @@ public class Plugin_DevAthlon extends JavaPlugin{
 			ghostspawn = p.getLocation();
 			p.sendMessage(ChatColor.GREEN + "Deine Aktuelle Position wurde als Spawnpunkt für die Geister gesetzt.");
 			return true;
+		} else if (commandlabel.equalsIgnoreCase("setMapCorner1")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Du bist kein Spieler!");
+				return false;
+			}
+			
+			if (args.length != 0) {
+				return false;
+			}
+			
+			Player p = (Player) sender;
+			mapcorner1 = p.getLocation();
+			
+			p.sendMessage(ChatColor.GREEN + "Deine Aktuelle Position wurde als Ecke der Map gesetzt.");
+			return true;
+		} else if (commandlabel.equalsIgnoreCase("setMapCorner2")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "Du bist kein Spieler!");
+				return false;
+			}
+			
+			if (args.length != 0) {
+				return false;
+			}
+			
+			Player p = (Player) sender;
+			mapcorner2 = p.getLocation();
+			
+			p.sendMessage(ChatColor.GREEN + "Deine Aktuelle Position wurde als Ecke der Map gesetzt.");
+			return true;
 		} else if (commandlabel.equalsIgnoreCase("startgame")) {
 			if (args.length != 2) {
 				sender.sendMessage(ChatColor.RED + "Das erste Argument muss die Anzahl der Spieler sein und das zweite Argument muss die Anzahl der Geister sein!");
@@ -128,19 +170,51 @@ public class Plugin_DevAthlon extends JavaPlugin{
 				return false;
 			}
 			
+			if (ghosts <= 0) {
+				sender.sendMessage(ChatColor.RED + "Es muss mindestends 1 Geist geben!");
+				return false;
+			}
+			if (players <= 0) {
+				sender.sendMessage(ChatColor.RED + "Es muss mindestends 1 Spieler geben1");
+				return false;
+			}
+			
 			if (playerspawns.size() < players) {
 				sender.sendMessage(ChatColor.RED + "Zu wenig Spieler Spawns!");
+				return false;
 			}
 			
 			if (ghostspawn == null) {
 				sender.sendMessage(ChatColor.RED + "Es wurde noch kein Geister Spawn gesetzt!");
+				return false;
 			}
 			
-			game = new Game(players, ghosts, playerspawns, ghostspawn);
+			if (Bukkit.getOnlinePlayers().size() < players+ghosts) {
+				sender.sendMessage(ChatColor.RED + "Es sind zu wenig Spieler online, es müssen mindestends " + (ghosts+players) + " Spieler online sein!");
+				return false;
+			}
+			
+			if (mapcorner1 == null || mapcorner2 == null) {
+				sender.sendMessage(ChatColor.RED + "Die Ecken der Map wurden noch nicht gesetzt!");
+				return false;
+			}
+			
+			game = new Game(players, ghosts, playerspawns, ghostspawn, mapcorner1, mapcorner2);
 			getServer().getPluginManager().registerEvents(game, this);
 			
 			sender.sendMessage(ChatColor.GREEN + "Das Spiel wurde mit " + players + " Spielern und " + ghosts + " Geistern gestartet!");
 			return true;
+		} else if (commandlabel.equalsIgnoreCase("stopgame")) {
+			if (game != null) {
+				game.stopGame();
+				game = null;
+				sender.sendMessage(ChatColor.GREEN + "Spiel wurde gestoppt!");
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.RED + "Es war kein Spiel gestartet!");
+				return true;
+			}
+			
 		}
 		
 		return false;
--- DevAthlon/src/de/nonamelabs/devathlon/paccraft/Game.java
+++ DevAthlon/src/de/nonamelabs/devathlon/paccraft/Game.java
@@ -9,15 +9,31 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.WeatherType;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import de.nonamelabs.devathlon.paccraft.items.Armor;
 import de.nonamelabs.devathlon.paccraft.items.Items;
 import de.nonamelabs.devathlon.paccraft.items.LootBoxen;
 import de.nonamelabs.devathlon.paccraft.items.Weapons;
@@ -26,44 +42,44 @@ public class Game implements Listener{
 	public static final ChatColor PLUGIN_COLOR = ChatColor.AQUA;
 	public static final ChatColor CHATCOLOR = ChatColor.WHITE;
 	public static final ChatColor PLUGIN_NAME_COLOR = ChatColor.DARK_PURPLE;
+	public static final Material COIN_MATERIAL = Material.GOLD_NUGGET;
+	public static final Material COIN_BOTTOM_BLOCK = Material.BARRIER;	
+	public static final Material COIN_BOTTOM_BLOCK2 = Material.WOOD;	
 	
-	List<Player> player_list = new ArrayList<Player>();
-	List<Player> ghost_list = new ArrayList<Player>();
-	List<Player> spectator_list = new ArrayList<Player>();
+	public List<Player> player_list = new ArrayList<Player>();
+	public List<Player> ghost_list = new ArrayList<Player>();
+	public List<Player> spectator_list = new ArrayList<Player>();
 	
-	int players;
-	int ghosts;
-	Location ghostspawn;
-	Random r = new Random();
-	public Game(int players, int ghosts, List<Location> playerspawns, Location ghostspawn) {
-		if (Bukkit.getOnlinePlayers().size() < (players+ghosts)) {
-			return;
-		}
-		
-		for (int i = 0; i < players; i++) {
-			for (Player p:Bukkit.getOnlinePlayers()) {
-				if (r.nextBoolean()) {
-					if (player_list.size() < players) {
-						player_list.add(p);
-						initPlayer(p, playerspawns.get(player_list.size()-1));
-					} else if (ghost_list.size() < ghosts) {
-						ghost_list.add(p);
-						initGhost(p, ghostspawn);
-					} else {
-						spectator_list.add(p);
-						initSpectator(p, ghostspawn);
-					}
+	public int players;
+	public int ghosts;
+	public Location ghostspawn;
+	public Random r = new Random();
+	public Location mapcorner1;
+	public Location mapcorner2;	
+	
+	public Game(int players, int ghosts, List<Location> playerspawns, Location ghostspawn, Location mapcorner1, Location mapcorner2) {
+		for (Player p:Bukkit.getOnlinePlayers()) {
+			if (r.nextBoolean()) {
+				if (player_list.size() < players) {
+					player_list.add(p);
+					initPlayer(p, playerspawns.get(player_list.size()-1));
+				} else if (ghost_list.size() < ghosts) {
+					ghost_list.add(p);
+					initGhost(p, ghostspawn);
 				} else {
-					if (ghost_list.size() < ghosts) {
-						ghost_list.add(p);
-						initGhost(p, ghostspawn);
-					} else if (player_list.size() < players) {
-						player_list.add(p);
-						initPlayer(p, playerspawns.get(player_list.size()-1));
-					} else {
-						spectator_list.add(p);
-						initSpectator(p, ghostspawn);
-					}
+					spectator_list.add(p);
+					initSpectator(p, ghostspawn);
+				}
+			} else {
+				if (ghost_list.size() < ghosts) {
+					ghost_list.add(p);
+					initGhost(p, ghostspawn);
+				} else if (player_list.size() < players) {
+					player_list.add(p);
+					initPlayer(p, playerspawns.get(player_list.size()-1));
+				} else {
+				spectator_list.add(p);
+					initSpectator(p, ghostspawn);
 				}
 			}
 		}
@@ -71,6 +87,30 @@ public class Game implements Listener{
 		this.players = players;
 		this.ghosts = ghosts;
 		this.ghostspawn = ghostspawn;
+		this.mapcorner1 = mapcorner1;
+		this.mapcorner2 = mapcorner2;
+		
+		initMap();
+	}
+	
+	public void initMap() {
+		int counter = 0;
+		for (int x = Math.min(mapcorner1.getBlockX(), mapcorner2.getBlockX()); x <= Math.max(mapcorner1.getBlockX(), mapcorner2.getBlockX()); x++) {
+			for (int y = Math.min(mapcorner1.getBlockY(), mapcorner2.getBlockY()); y <= Math.max(mapcorner1.getBlockY(), mapcorner2.getBlockY()); y++) {
+				for (int z = Math.min(mapcorner1.getBlockZ(), mapcorner2.getBlockZ()); z <= Math.max(mapcorner1.getBlockZ(), mapcorner2.getBlockZ()); z++) {
+					if (mapcorner1.getWorld().getBlockAt(x, y, z).getType() == Material.AIR && (mapcorner1.getWorld().getBlockAt(x, y-1, z).getType() == COIN_BOTTOM_BLOCK || mapcorner1.getWorld().getBlockAt(x, y-1, z).getType() == COIN_BOTTOM_BLOCK2)) {
+						if (r.nextBoolean() && r.nextBoolean()) {
+							ItemStack coin = new ItemStack(COIN_MATERIAL, 1);
+							ItemMeta meta = coin.getItemMeta();
+							meta.setDisplayName(counter + "");
+							counter++;
+							coin.setItemMeta(meta);
+							mapcorner1.getWorld().dropItem(new Location(mapcorner1.getWorld(), x + 0.5, y, z + 0.5), coin);
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	public void initPlayer(Player p, Location l) {
@@ -78,9 +118,19 @@ public class Game implements Listener{
 		p.setGameMode(GameMode.ADVENTURE);
 		p.setHealth(20);
 		p.spigot().respawn();
-		p.getInventory().clear();
 		p.setFoodLevel(20);
-		p.getInventory().setItem(8, Items.Shop());		
+		p.setExp(0);
+		p.setLevel(0);
+		p.setPlayerWeather(WeatherType.DOWNFALL);
+		p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999, 4));
+		p.setDisplayName(ChatColor.YELLOW + p.getName());
+		p.setPlayerListName(ChatColor.YELLOW + p.getName());
+		
+		PlayerInventory inv = p.getInventory();	
+		inv.clear();	
+		inv.setArmorContents(new ItemStack[] {Armor.PlayerBoots(), Armor.PlayerLegs(), Armor.PlayerChest(), Armor.PlayerHelm()});
+		inv.setItem(8, Items.Shop());
+		
 		sendGameMessage("Du bist ein Spieler!", p);
 	}
 	
@@ -89,9 +139,19 @@ public class Game implements Listener{
 		p.setGameMode(GameMode.ADVENTURE);
 		p.setHealth(20);
 		p.spigot().respawn();
-		p.getInventory().clear();
 		p.setFoodLevel(20);
-		p.getInventory().setItem(8, Items.Shop());		
+		p.setExp(0);
+		p.setLevel(0);
+		p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 999999, 2));
+		p.setDisplayName(ChatColor.WHITE + p.getName());
+		p.setPlayerListName(ChatColor.WHITE + p.getName());
+
+		PlayerInventory inv = p.getInventory();	
+		inv.clear();	
+		inv.setArmorContents(new ItemStack[] {Armor.GhostBoots(), Armor.GhostLegs(), Armor.GhostChest(), Armor.GhostHelm()});
+		inv.setItem(8, Items.Shop());
+		
+		
 		sendGameMessage("Du bist ein Geist!", p);
 	}
 	
@@ -102,6 +162,8 @@ public class Game implements Listener{
 		p.spigot().respawn();
 		p.getInventory().clear();
 		p.setFoodLevel(20);
+		p.setExp(0);
+		p.setLevel(0);
 		sendGameMessage("Das Spiel ist bereits voll! Du bist nun ein Spectator!", p);
 	}
 	
@@ -113,11 +175,42 @@ public class Game implements Listener{
 		p.sendMessage(PLUGIN_COLOR + "[" + PLUGIN_NAME_COLOR + "PacCraft" + PLUGIN_COLOR + "] " + message);
 	}
 	
+	public void stopGame() {
+		HandlerList.unregisterAll(this);
+		
+		for (Entity e:mapcorner1.getWorld().getEntities()) {
+			if (e instanceof Item) {
+				e.remove();
+			}
+		}
+		
+		for (Player p: Bukkit.getOnlinePlayers()) {
+			p.setDisplayName(p.getName());
+			p.setPlayerListName(p.getDisplayName());
+			p.setPlayerWeather(WeatherType.CLEAR);
+			p.removePotionEffect(PotionEffectType.BLINDNESS);
+			p.getInventory().clear();
+		}
+	}
 	//Events-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		spectator_list.add(event.getPlayer());
 		initSpectator(event.getPlayer(), ghostspawn);
+	}
+	
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent event) {
+		event.setQuitMessage("");
+		if (ghost_list.contains(event.getPlayer())) {
+			ghost_list.remove(event.getPlayer());
+			sendGameMessage(event.getPlayer().getName() + " hat das Spiel verlassen! Es gibt noch " + ghost_list.size() + " Geister!");
+		} else if (player_list.contains(event.getPlayer())) {
+			player_list.remove(event.getPlayer());
+			sendGameMessage(event.getPlayer().getName() + " hat das Spiel verlassen! Es gibt noch " + player_list.size() + " Spieler!");
+		} else {
+			spectator_list.remove(event.getPlayer());
+		}
 	}
 	
 	
@@ -159,4 +252,46 @@ public class Game implements Listener{
 		}
 	}
 	
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent event) {
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onPlayerLooseHunger(FoodLevelChangeEvent event) {
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onPlayerDrop(PlayerDropItemEvent event) {
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		if (event.getMessage().contains("%")) {
+			event.setCancelled(true);
+			return;
+		}
+
+		if (spectator_list.contains(event.getPlayer())) {
+			event.setCancelled(true);
+			sendGameMessage("Du bist Spectator, du kannst nichts in den Chat schreiben", event.getPlayer());
+			return;
+		}
+		
+		Player p = event.getPlayer();
+		String color = ChatColor.getLastColors(p.getDisplayName());
+		event.setFormat(color + "%1$s" + CHATCOLOR + ": " + "%2$s");
+	}
+	
+	@EventHandler
+	public void onPlayerPickup(PlayerPickupItemEvent event) {
+		Player p = event.getPlayer();
+		if (player_list.contains(p) && event.getItem() != null && event.getItem().getItemStack().getType() == COIN_MATERIAL) {
+			p.playSound(p.getLocation(), Sound.LEVEL_UP, 1, 1);
+			event.getItem().remove();
+		}
+		event.setCancelled(true);
+	}	
 }
