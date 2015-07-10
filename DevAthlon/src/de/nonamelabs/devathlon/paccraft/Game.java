11a2167da988a9a8ea2f68aa8122ee26ce9c8f97
Lau <NoNameLabs42@googlemail.com>
2015-07-10 21:07:52 +0200

    Push

--- DevAthlon/src/de/nonamelabs/devathlon/paccraft/Game.java
+++ DevAthlon/src/de/nonamelabs/devathlon/paccraft/Game.java
@@ -2,8 +2,10 @@ package de.nonamelabs.devathlon.paccraft;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 import de.nonamelabs.devathlon.paccraft.items.Items;
@@ -20,9 +23,18 @@ import de.nonamelabs.devathlon.paccraft.items.LootBoxen;
 import de.nonamelabs.devathlon.paccraft.items.Weapons;
 
 public class Game implements Listener{
+	public static final ChatColor PLUGIN_COLOR = ChatColor.AQUA;
+	public static final ChatColor CHATCOLOR = ChatColor.WHITE;
+	public static final ChatColor PLUGIN_NAME_COLOR = ChatColor.DARK_PURPLE;
+	
 	List<Player> player_list = new ArrayList<Player>();
 	List<Player> ghost_list = new ArrayList<Player>();
 	List<Player> spectator_list = new ArrayList<Player>();
+	
+	int players;
+	int ghosts;
+	Location ghostspawn;
+	Random r = new Random();
 	public Game(int players, int ghosts, List<Location> playerspawns, Location ghostspawn) {
 		if (Bukkit.getOnlinePlayers().size() < (players+ghosts)) {
 			return;
@@ -30,18 +42,35 @@ public class Game implements Listener{
 		
 		for (int i = 0; i < players; i++) {
 			for (Player p:Bukkit.getOnlinePlayers()) {
-				if (player_list.size() < players) {
-					player_list.add(p);
-					initPlayer(p, playerspawns.get(player_list.size()-1));
-				} else if (ghost_list.size() < ghosts) {
-					ghost_list.add(p);
-					initGhost(p, ghostspawn);
+				if (r.nextBoolean()) {
+					if (player_list.size() < players) {
+						player_list.add(p);
+						initPlayer(p, playerspawns.get(player_list.size()-1));
+					} else if (ghost_list.size() < ghosts) {
+						ghost_list.add(p);
+						initGhost(p, ghostspawn);
+					} else {
+						spectator_list.add(p);
+						initSpectator(p, ghostspawn);
+					}
 				} else {
-					spectator_list.add(p);
-					initSpectator(p, ghostspawn);
+					if (ghost_list.size() < ghosts) {
+						ghost_list.add(p);
+						initGhost(p, ghostspawn);
+					} else if (player_list.size() < players) {
+						player_list.add(p);
+						initPlayer(p, playerspawns.get(player_list.size()-1));
+					} else {
+						spectator_list.add(p);
+						initSpectator(p, ghostspawn);
+					}
 				}
 			}
 		}
+		
+		this.players = players;
+		this.ghosts = ghosts;
+		this.ghostspawn = ghostspawn;
 	}
 	
 	public void initPlayer(Player p, Location l) {
@@ -51,7 +80,8 @@ public class Game implements Listener{
 		p.spigot().respawn();
 		p.getInventory().clear();
 		p.setFoodLevel(20);
-		p.getInventory().setItem(8, Items.Shop());
+		p.getInventory().setItem(8, Items.Shop());		
+		sendGameMessage("Du bist ein Spieler!", p);
 	}
 	
 	public void initGhost(Player p, Location l) {
@@ -62,6 +92,7 @@ public class Game implements Listener{
 		p.getInventory().clear();
 		p.setFoodLevel(20);
 		p.getInventory().setItem(8, Items.Shop());		
+		sendGameMessage("Du bist ein Geist!", p);
 	}
 	
 	public void initSpectator(Player p, Location l) {
@@ -71,19 +102,33 @@ public class Game implements Listener{
 		p.spigot().respawn();
 		p.getInventory().clear();
 		p.setFoodLevel(20);
-		
+		sendGameMessage("Das Spiel ist bereits voll! Du bist nun ein Spectator!", p);
+	}
+	
+	public void sendGameMessage(String message) {
+		Bukkit.broadcastMessage(PLUGIN_COLOR + "[" + PLUGIN_NAME_COLOR + "PacCraft" + PLUGIN_COLOR + "] " + message);
+	}
+	
+	public void sendGameMessage(String message, Player p) {
+		p.sendMessage(PLUGIN_COLOR + "[" + PLUGIN_NAME_COLOR + "PacCraft" + PLUGIN_COLOR + "] " + message);
+	}
+	
+	//Events-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		spectator_list.add(event.getPlayer());
+		initSpectator(event.getPlayer(), ghostspawn);
 	}
 	
 	
-	
-	//Events-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 	@EventHandler
 	public void onPlayerClick(PlayerInteractEvent event){
+		event.setCancelled(true);
+		
 		if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getItem() != null) {
 			if (event.getItem().equals((Items.Shop()))) {
 				event.setCancelled(true);
-				event.getPlayer().openInventory(LootBoxen.Shop);
+				event.getPlayer().openInventory(LootBoxen.getShopInventory());
 			}
 		}
 	}
@@ -98,8 +143,8 @@ public class Game implements Listener{
 			if (clicked_item.equals(LootBoxen.Nahrung())) {
 				p.getInventory().addItem(new ItemStack(Material.COOKED_CHICKEN));			
 			}else if(clicked_item.equals(LootBoxen.getArmorUpgrade())){
-				event.getWhoClicked().openInventory(LootBoxen.ArmorUpgrade);
-			}else if(clicked_item.equals(LootBoxen.getWeaponUpgrade(p))){
+				event.getWhoClicked().openInventory(LootBoxen.getArmorUpgradeInventory());
+			}else if(clicked_item.equals(LootBoxen.getWeaponUpgrade())){
 				if(p.getInventory().contains(Weapons.WoodenSword())){
 					p.getInventory().remove(Weapons.WoodenSword());
 					p.getInventory().addItem(Weapons.StoneSword());		
--- DevAthlon/src/de/nonamelabs/devathlon/paccraft/items/LootBoxen.java
+++ DevAthlon/src/de/nonamelabs/devathlon/paccraft/items/LootBoxen.java
@@ -6,19 +6,16 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-
 
 public class LootBoxen {
 	public static Inventory Shop = Bukkit.createInventory(null, 9, ChatColor.RED + "Kaufe Nahrung, Tränke, verbesser deine Rüstung und Waffen");
 	static {
 		Shop.setItem(1, new ItemStack(Nahrung()));
 		Shop.setItem(4, new ItemStack(getArmorUpgrade()));
-		Shop.setItem(7, new ItemStack(getWeaponUpgrade(null)));
+		Shop.setItem(7, new ItemStack(getWeaponUpgrade()));
 	}
 	
 	public static Inventory ArmorUpgrade = Bukkit.createInventory(null, 9, ChatColor.RED + "Verbessere deine Rüstung");
@@ -29,6 +26,14 @@ public class LootBoxen {
 		ArmorUpgrade.setItem(7, new ItemStack(Material.DIAMOND_BOOTS));
 	}
 	
+	
+	public static Inventory getShopInventory() {
+		return Shop;
+	}
+	
+	public static Inventory getArmorUpgradeInventory() {
+		return ArmorUpgrade;
+	}
 	
 	//LootItems
 	public static ItemStack Nahrung() {
@@ -44,14 +49,10 @@ public class LootBoxen {
 		return item;
 	}
 	
-	public static ItemStack getWeaponUpgrade(Player p) {
+	public static ItemStack getWeaponUpgrade() {
 		ItemStack item = new ItemStack(Material.DIAMOND_SWORD);
 		List<String> lore = new ArrayList<String>();
-		lore.add(ChatColor.GREEN + "Knüppel sie alle nieder!");
-		if(p.getInventory().contains(Weapons.WoodenSword())) lore.add(ChatColor.GREEN + "Kosten: 50");
-		if(p.getInventory().contains(Weapons.StoneSword())) lore.add(ChatColor.GREEN + "Kosten: 250");
-		if(p.getInventory().contains(Weapons.IronSword())) lore.add(ChatColor.GREEN + "Kosten: 600");
-		
+		lore.add(ChatColor.GREEN + "Knüppel sie alle nieder!");		
 		
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(ChatColor.BLUE + "Verbessert deine Waffe");
@@ -60,6 +61,7 @@ public class LootBoxen {
 
 		return item;
 	}
+
 	
 	public static ItemStack getArmorUpgrade() {
 		ItemStack item = new ItemStack(Material.DIAMOND_CHESTPLATE);
