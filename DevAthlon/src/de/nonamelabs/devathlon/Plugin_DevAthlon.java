c658b63807f954253d8e47d085ff757bcb391093
Lau <NoNameLabs42@googlemail.com>
2015-07-10 17:49:02 +0200

    First Class System

--- DevAthlon/src/de/nonamelabs/devathlon/Plugin_DevAthlon.java
+++ DevAthlon/src/de/nonamelabs/devathlon/Plugin_DevAthlon.java
@@ -2,6 +2,9 @@ package de.nonamelabs.devathlon;
 
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin_DevAthlon extends JavaPlugin{
@@ -18,4 +21,11 @@ public class Plugin_DevAthlon extends JavaPlugin{
 		logger.info("Plugin wurde gestoppt!");
 	}
 	
+	
+	public boolean onCommand(CommandSender sender, Command command, String commandlabel, String[] args) {
+		if (commandlabel.equalsIgnoreCase("startgame")) {
+			sender.sendMessage(ChatColor.GREEN + "Das Spiel wurde gestartet!");
+		}
+		return false;
+	}
 }
---
+++ DevAthlon/src/de/nonamelabs/devathlon/paccraft/Lobby.java
@@ -0,0 +1,5 @@
+package de.nonamelabs.devathlon.paccraft;
+
+public class Lobby {
+
+}
